10cad58a5f637abedf865e9f8e6f9c6b812483c1
Chris Travers <chris@efficito.com>
2014-09-20 20:23:37 -0700

    Final triggers synchronizing changes from opos to lsmb.

--- sql/setup3.sql
+++ sql/setup3.sql
@@ -1,3 +1,8 @@
+-- Copyright Efficito, Ltd.
+-- Licensed under the GNU General Public License version 2 or at your option
+-- any later version.  Please see the included licenses/gpl2.txt for more 
+-- information.
+
 BEGIN;
 
 CREATE SCHEMA opos_integration;
@@ -14,24 +19,40 @@ $$
 DECLARE
   join_rec parts_opos;
 BEGIN
-  SELECT * INTO join_rec FROM parts_opos WHERE product_id = new.id;
+  SELECT * INTO join_rec FROM opos_integration.parts_opos 
+   WHERE product_id = new.id;
+
   IF NOT FOUND THEN
-     INSERT INTO parts ....
-     (...);
+     INSERT INTO opos_integration.parts_opos
+     VALUES (nextval('parts_id_seq'), new.id, true);
+
+     INSERT INTO parts 
+            (id, partnumber, description, 
+            inventory_accno_id, expense_accno_id, income_accno_id,
+            sellprice, lastcost)
+     VALUES (currval('parts_id_seq'), new.code, new.name,
+             (setting_get('inventory_accno_id')).value,
+             (setting_get('expense_accno_id')).value,
+             (setting_get('income_accno_id')).value,
+             new.pricesell, new.pricebuy);
 
      UPDATE parts_opos SET being_written = false WHERE product_id = new.id;
   ELSE
      IF join_rec.being_written IS TRUE THEN
          return new;
      END IF;
+
      UPDATE parts_opos SET being_written = true WHERE product_id = new.id;
-     UPDATE parts ....;
+     UPDATE parts 
+        SET partnumber = new.code,
+            description = new.name,
+            sellprice = new.pricesell
+      WHERE id = join_rec.parts_id;
 
      UPDATE parts_opos SET being_written = false WHERE product_id = new.id;
 $$;
 
 CREATE TRIGGER sync_lsmb AFTER INSERT TO products
 FOR EACH ROW EXECUTE PROCEDURE opos_integration.opos_sync_parts();
-
 
 COMMIT;
